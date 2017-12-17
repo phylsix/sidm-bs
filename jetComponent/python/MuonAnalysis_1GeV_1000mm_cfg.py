0adb3dfc2e343c304b4df5fb00a3659bbb1559dd
Weinan <siweinan@gmail.com>
2017-12-16 21:11:16 -0600

    add all muon analysis config files

---
+++ jetComponent/python/MuonAnalysis_1GeV_1000mm_cfg.py
@@ -0,0 +1,38 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("MuonAnalysis")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.cerr.threshold = 'ERROR'
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.options   = cms.untracked.PSet( SkipEvent = cms.untracked.vstring('ProductNotFound'))
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring([
+        'root://cmseos.fnal.gov//store/user/wsi/ZpMuMu_1GeV_2e-14GeV/ZpMuMu_1GeV_2e-14GeV_RECO_MINIAOD/171128_030539/0000/step3_inMINIAODSIM_1.root',
+        'root://cmseos.fnal.gov//store/user/wsi/ZpMuMu_1GeV_2e-14GeV/ZpMuMu_1GeV_2e-14GeV_RECO_MINIAOD/171128_030539/0000/step3_inMINIAODSIM_10.root',
+        'root://cmseos.fnal.gov//store/user/wsi/ZpMuMu_1GeV_2e-14GeV/ZpMuMu_1GeV_2e-14GeV_RECO_MINIAOD/171128_030539/0000/step3_inMINIAODSIM_2.root',
+        'root://cmseos.fnal.gov//store/user/wsi/ZpMuMu_1GeV_2e-14GeV/ZpMuMu_1GeV_2e-14GeV_RECO_MINIAOD/171128_030539/0000/step3_inMINIAODSIM_3.root',
+        'root://cmseos.fnal.gov//store/user/wsi/ZpMuMu_1GeV_2e-14GeV/ZpMuMu_1GeV_2e-14GeV_RECO_MINIAOD/171128_030539/0000/step3_inMINIAODSIM_4.root',
+        'root://cmseos.fnal.gov//store/user/wsi/ZpMuMu_1GeV_2e-14GeV/ZpMuMu_1GeV_2e-14GeV_RECO_MINIAOD/171128_030539/0000/step3_inMINIAODSIM_5.root',
+        'root://cmseos.fnal.gov//store/user/wsi/ZpMuMu_1GeV_2e-14GeV/ZpMuMu_1GeV_2e-14GeV_RECO_MINIAOD/171128_030539/0000/step3_inMINIAODSIM_6.root',
+        'root://cmseos.fnal.gov//store/user/wsi/ZpMuMu_1GeV_2e-14GeV/ZpMuMu_1GeV_2e-14GeV_RECO_MINIAOD/171128_030539/0000/step3_inMINIAODSIM_7.root',
+        'root://cmseos.fnal.gov//store/user/wsi/ZpMuMu_1GeV_2e-14GeV/ZpMuMu_1GeV_2e-14GeV_RECO_MINIAOD/171128_030539/0000/step3_inMINIAODSIM_8.root',
+        'root://cmseos.fnal.gov//store/user/wsi/ZpMuMu_1GeV_2e-14GeV/ZpMuMu_1GeV_2e-14GeV_RECO_MINIAOD/171128_030539/0000/step3_inMINIAODSIM_9.root'
+    ])
+)
+
+process.MuonAnalysis = cms.EDAnalyzer("sidm::MuonAnalysis",
+            GenParticleTag_ = cms.untracked.InputTag("prunedGenParticles"),
+              PatPkdGenTag_ = cms.untracked.InputTag("packedGenParticles"),
+                  PatPfTag_ = cms.untracked.InputTag("packedPFCandidates"),
+            PatElectronTag_ = cms.untracked.InputTag("slimmedElectrons"),
+                PatMuonTag_ = cms.untracked.InputTag("slimmedMuons"),
+                 PatJetTag_ = cms.untracked.InputTag("slimmedJets"),
+)
+process.TFileService = cms.Service("TFileService",
+                                   fileName
+                                   = cms.string("$CMSSW_BASE/src/sidm-bs/jetComponent/mydata/MuonAnalysis_1GeV_1000mm.root"))
+
+process.p = cms.Path(process.MuonAnalysis)
---
+++ jetComponent/python/MuonAnalysis_1GeV_100mm_cfg.py
@@ -0,0 +1,38 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("MuonAnalysis")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.cerr.threshold = 'ERROR'
+
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.options   = cms.untracked.PSet( SkipEvent = cms.untracked.vstring('ProductNotFound'))
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring([
+        'root://cmseos.fnal.gov//store/user/wsi/ZpMuMu_1GeV_2e-13GeV/ZpMuMu_1GeV_2e-13GeV_RECO_MINIAOD/171127_211145/0000/step3_inMINIAODSIM_1.root',
+        'root://cmseos.fnal.gov//store/user/wsi/ZpMuMu_1GeV_2e-13GeV/ZpMuMu_1GeV_2e-13GeV_RECO_MINIAOD/171127_211145/0000/step3_inMINIAODSIM_10.root',
+        'root://cmseos.fnal.gov//store/user/wsi/ZpMuMu_1GeV_2e-13GeV/ZpMuMu_1GeV_2e-13GeV_RECO_MINIAOD/171127_211145/0000/step3_inMINIAODSIM_2.root',
+        'root://cmseos.fnal.gov//store/user/wsi/ZpMuMu_1GeV_2e-13GeV/ZpMuMu_1GeV_2e-13GeV_RECO_MINIAOD/171127_211145/0000/step3_inMINIAODSIM_3.root',
+        'root://cmseos.fnal.gov//store/user/wsi/ZpMuMu_1GeV_2e-13GeV/ZpMuMu_1GeV_2e-13GeV_RECO_MINIAOD/171127_211145/0000/step3_inMINIAODSIM_4.root',
+        'root://cmseos.fnal.gov//store/user/wsi/ZpMuMu_1GeV_2e-13GeV/ZpMuMu_1GeV_2e-13GeV_RECO_MINIAOD/171127_211145/0000/step3_inMINIAODSIM_5.root',
+        'root://cmseos.fnal.gov//store/user/wsi/ZpMuMu_1GeV_2e-13GeV/ZpMuMu_1GeV_2e-13GeV_RECO_MINIAOD/171127_211145/0000/step3_inMINIAODSIM_6.root',
+        'root://cmseos.fnal.gov//store/user/wsi/ZpMuMu_1GeV_2e-13GeV/ZpMuMu_1GeV_2e-13GeV_RECO_MINIAOD/171127_211145/0000/step3_inMINIAODSIM_7.root',
+        'root://cmseos.fnal.gov//store/user/wsi/ZpMuMu_1GeV_2e-13GeV/ZpMuMu_1GeV_2e-13GeV_RECO_MINIAOD/171127_211145/0000/step3_inMINIAODSIM_8.root',
+        'root://cmseos.fnal.gov//store/user/wsi/ZpMuMu_1GeV_2e-13GeV/ZpMuMu_1GeV_2e-13GeV_RECO_MINIAOD/171127_211145/0000/step3_inMINIAODSIM_9.root'
+    ])
+)
+
+process.MuonAnalysis = cms.EDAnalyzer("sidm::MuonAnalysis",
+            GenParticleTag_ = cms.untracked.InputTag("prunedGenParticles"),
+              PatPkdGenTag_ = cms.untracked.InputTag("packedGenParticles"),
+                  PatPfTag_ = cms.untracked.InputTag("packedPFCandidates"),
+            PatElectronTag_ = cms.untracked.InputTag("slimmedElectrons"),
+                PatMuonTag_ = cms.untracked.InputTag("slimmedMuons"),
+                 PatJetTag_ = cms.untracked.InputTag("slimmedJets"),
+)
+process.TFileService = cms.Service("TFileService",
+                                   fileName
+                                   = cms.string("$CMSSW_BASE/src/sidm-bs/jetComponent/mydata/MuonAnalysis_1GeV_100mm.root"))
+
+process.p = cms.Path(process.MuonAnalysis)
